994e582dd0cfff5ca2b7ce674b05762afaa0c77a
Patrick DeVivo <patrick.devivo@gmail.com>
2011-12-02 21:10:47 -0500

    start adding some classes

---
+++ public/js/classes/spring_2012.js
@@ -0,0 +1,34 @@
+// Class Data
+var classes = new Array(
+	[1, "Studio Art", "Liu", 2, days = [false, "t", "w", "r", false], 201],
+	[1, "Intro to Photography", "Scarville", 3, days = [false, false, false, "r", false], 404],
+	[2, "Intro to Photography", "Scarville", 6, days = [false, false, false, "r", false], 404],
+	[1, "Advanced Photography", "Scarville", 5, days = [false, false, false, "r", false], 404],
+	[1, "Art History II", "Martin", 6, days = ["m", false, false, "r", "f"], 504],
+	[1, "Dance Ensemble", "Marasigan", 4, days = [false, "t", false, "r", "f"], 'Gym'],	
+	[1, "Capoeira", "Anaya", 0, days = [false, "t", false, "r", false], 'Aud'],
+	[1, "Painting", "Casey", 1, days = ["m", false, "w", false, "f"], 201],
+	[2, "Painting", "Casey", 3, days = ["m", false, "w", false, "f"], 201],
+	[1, "Theater Practicum", "Tibbels Jordan", 2, days = ["m", "t", false, "r", "f"], 'Aud'],
+	[1, "World Drumming", "Healy", 8, days = [false, "t", false, "t",false], 309],
+	[1, "BHSEC Chorus Coll", "Moffett", 3, days = [false, "t", false, "r",false], 208],
+	[1, "Rock Music Ensemble", "Scarim", 6, days = [false, "t", false, "r",false], 208],
+	[1, "Chamber Orchestra", "Scarim", 8, days = [false, "t", false, "r",false], 208],
+
+	
+	[1, "First Year Seminar II", "Martin", 1, days = ["m", "t", false, "r", "f"], 413],
+	[2, "First Year Seminar II", "Birch", 1, days = ["m", "t", false, "r", "f"], 501],
+	[3, "First Year Seminar II", "Mazie", 1, days = ["m", "t", false, "r", "f"], 503],
+	[4, "First Year Seminar II", "Martin", 4, days = ["m", false, "w", "r", "f"], 413],
+	[5, "First Year Seminar II", "Birch", 4, days = ["m", "t", false, "r", "f"], 501],
+	[6, "First Year Seminar II", "Mazie", 1, days = ["m", "t", false, "r", "f"], 511],
+	
+	[1, "Sophomore Seminar II", "Cho", 1, days = ["m", "t", false, "r", "f"], 502],
+	[2, "Sophomore Seminar II", "Johnson", 2, days = ["m", false, "w", "r", "f"], 501],
+	[3, "Sophomore Seminar II", "Scott", 1, days = ["m", false, "w", "r", "f"], 502],
+	[4, "Sophomore Seminar II", "Cho", 4, days = ["m", "t", false, "r", "f"], 502],
+	[5, "Sophomore Seminar II", "Vartorella", 4, days = ["m", "t", "w", false, "f"], 503],
+	[6, "Sophomore Seminar II", "Scott", 4, days = ["m", false, "w", "r", "f"], 302],
+	[7, "Sophomore Seminar II", "Lerner", 4, days = ["m", "t", "w", "r", false], 202]
+	
+);
---
+++ public/js/main.js
@@ -0,0 +1,143 @@
+"use strict";	
+// Functions
+function translate(course_code) {
+	var i = 0;
+	var payload = new Array()
+	payload = course_code.split(",", 3); // ["4", "Year 1 Seminar"]
+	return payload;
+}
+
+
+function list() {
+	$('.block').each(function(period) {
+		$(this).append('<h1 id="period-'+ (period) +'">Period ' + (period) + ' Classes</h1>');
+		var i = 0;
+		while (i<=classes.length - 1) {
+			var course_code = translate(classes[i].toString());
+			if (classes[i][3] == period) {
+				$(this).append('<span class="class-selectors" type="checkbox" name="' + classes[i] + '" title="Meets ' + classes[i][4].toString().replace(/false/gi, "").replace(/,/gi, "").toUpperCase() + ' (in ' + classes[i][5] +')">' + classes[i][1] + ', ' + classes[i][2] + '</span><br />');
+				//console.log(course_code);
+			}	
+	
+		i++
+		}
+		});
+}
+
+function insert(course_code) {
+	var i = 0;
+	course_code = translate(course_code);
+	while (i<=classes.length - 1) {
+		if (course_code[0] == classes[i][0] && course_code[1] == classes[i][1] && course_code[2] == classes[i][2]) {
+				//cycle through days
+				var d;
+				for(d=0;d<=classes[i][4].length - 1;d++) {
+					if (classes[i][4][d] != false ) {
+						$('#' + classes[i][4][d] + classes[i][3]).append("<p title='" + classes[i] + "'>"+classes[i][1]+"</p>");
+					}
+				}
+		}
+		i++
+	}
+	$('.class-selectors').each(function() {
+		var name = $(this).attr('name');
+		name = translate(name);
+		if (name[0] == course_code[0] && name[1] == course_code[1] && name[2] == course_code[2]) {
+			$(this).addClass('selected');
+		}
+	});
+}
+
+function remove(course_code) {
+	var i = 0;
+	course_code = translate(course_code);
+	while (i<=classes.length - 1) {
+		if (course_code[0] == classes[i][0] && course_code[1] == classes[i][1] && course_code[2] == classes[i][2]) {
+				//cycle through days
+				var d;
+				for(d=0;d<=classes[i][4].length - 1;d++) {
+					if (classes[i][4][d] != false ) {
+						$("#" + classes[i][4][d] + classes[i][3] + " p'[title=" + classes[i] + "]'").remove();
+					}
+				}
+		}
+		i++
+	}
+	$('.class-selectors').each(function() {
+		var name = $(this).attr('name');
+		name = translate(name);
+		if (name[0] == course_code[0] && name[1] == course_code[1] && name[2] == course_code[2]) {
+			$(this).removeClass('selected');
+		}
+	});
+}
+
+function check() {
+	$('td').each(function() {
+		var length = $(this).children().length;
+		if (length > 1) {
+			$(this).addClass('conflict');
+		}
+		else if (length <= 1 && $(this).hasClass('conflict') == true) {
+			$(this).removeClass('conflict');	
+		}
+	});
+}
+
+function creditCount() {
+	var count = 0;
+	$('.checked').each(function() {
+		
+	});
+	return count;
+}
+
+//$('#main').append("<input type='checkbox' name='" + classes[i] + "'>" + title + ", " + teacher + "</input><br />");
+$(document).ready(function() {
+	list(); //list all the classes
+
+	$('#start-over').click(function() {
+		$('td').each(function() {
+			$(this).children().remove();
+			$(this).removeClass('conflict');
+		})
+		$('.class-selectors').each(function() {
+			$(this).removeClass('selected');							   
+		})
+	});
+	
+	$('#print').click(function() {
+		$('#schedule').printElement({pageTitle:'Schedule.html'}); 			
+	});
+	
+	$('#table-block').scrollFollow({
+		easing: 'easeOutElastic',
+		offset: 20
+
+	});
+	
+	
+	$('.class-selectors').click(function() {
+		var name = $(this).attr('name');
+		if ($(this).hasClass('selected') == false) {
+			insert(name);
+			check();
+		}
+		else {
+			remove(name);
+			$(this).removeClass('selected');
+			check();
+		}
+	});
+	
+	$('span.class-selectors').qtip({
+		position: {
+		corner: {
+			target: 'leftMiddle',
+				tooltip: 'rightMiddle'
+		}
+   }
+
+				   
+	})
+});
